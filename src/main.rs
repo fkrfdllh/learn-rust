f1610f3454af4036f9a7c3d96f61073043fa2506
Fikri Fadillah <fkrfdllh@gmail.com>
2024-12-02 10:23:53 +0700

    refactor: - using vector (array explanation includes) - mutable & immutable variable - using format!() string - format output using {:#?}

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,35 @@ struct Deck {
     cards: Vec<String>
 }
 
+/*
+ * array vs Vector
+ * array just using `[]`
+ * vector using `vec![]`
+ * 
+ * array is in fixed size
+ * vector's size can be dynamic
+ */
+
 fn main() {
-    // let deck = Deck { cards: Vec::new() };
-    let deck = Deck { cards: vec![] };
+    let suits  = vec!["Hearts", "Spades", "Diamonds"];
+    let values = vec!["Ace", "Two", "Three"];
     
-    println!("Here's your deck: {:?}", deck);
+    /*
+     * let variable = value <-- this is immutable, means that variable can't be reassigned (just like constant variable)
+     * let mut variable = value <-- this variable is mutable, the value can be changed
+     */
+    
+    let mut cards = vec![];
+    
+    for suit in suits {
+        for value in &values {
+            let card = format!("{} of {}", value, suit);
+            cards.push(card);
+        }
+    }
+    
+    // let deck = Deck { cards: cards };
+    let deck = Deck { cards };
+    
+    println!("Here's your deck: {:#?}", deck);
 }
