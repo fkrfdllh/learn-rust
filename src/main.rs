b4a1f3f205af9b019ae1765cd127f0b34c37628c
Fikri Fadillah <fkrfdllh@gmail.com>
2024-12-06 12:49:47 +0700

    define Account and Bank structs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+#[derive(Debug)]
+struct Account {
+    id: u128,
+    balance: i32,
+    holder: String,
+}
+
+#[derive(Debug)]
+struct Bank {
+    accounts: Vec<Account>,
+}
+
 fn main() {
     println!("Hello, world!");
 }
