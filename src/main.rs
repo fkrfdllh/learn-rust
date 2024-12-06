8ac4b60462449028bc7926f46932c2415dd53285
Fikri Fadillah <fkrfdllh@gmail.com>
2024-12-06 12:54:42 +0700

    define inherit assoc functions for Bank and Account structs

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,22 @@ struct Bank {
     accounts: Vec<Account>,
 }
 
+impl Bank {
+    fn new() -> Self {
+        Bank { accounts: vec![] }
+    }
+}
+
+impl Account {
+    fn new(id: u128, holder: String) -> Self {
+        Account {
+            id,
+            holder,
+            balance: 0,
+        }
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
