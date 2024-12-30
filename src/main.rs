f3351069e8e04ff2a84fcadd63b919cbd236c112
Fikri Fadillah <fkrfdllh@gmail.com>
2024-12-30 09:49:14 +0700

    ownership: (option 2) borrowing/reference

--- src/main.rs
+++ src/main.rs
@@ -26,20 +26,15 @@ impl Account {
     }
 }
 
-fn print_account(account: Account) -> Account {
+fn print_account(account: &Account) {
     println!("{:#?}", account);
-    account
-}
-
-fn print_holder(holder: String) {
-    println!("{}", holder);
 }
 
 fn main() {
-    let mut account = Account::new(1, String::from("me"));
+    let account = Account::new(1, String::from("me"));
+    let account_ref = &account;
 
-    account = print_account(account);
-    account = print_account(account);
+    print_account(account_ref);
 
     println!("{:#?}", account)
 }
