36f8221729b09d352a4bc428b74e69931785c7a6
Fikri Fadillah <fkrfdllh@gmail.com>
2025-02-11 15:28:07 +0700

    add summary and total_balance method (wrapping up)

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,17 @@ impl Bank {
     fn add_account(&mut self, account: Account) {
         self.accounts.push(account)
     }
+
+    fn total_balance(&self) -> i32 {
+        self.accounts.iter().map(|account| account.balance).sum()
+    }
+
+    fn summary(&self) -> Vec<String> {
+        self.accounts
+            .iter()
+            .map(|account| account.summary())
+            .collect::<Vec<String>>()
+    }
 }
 
 impl Account {
@@ -27,6 +38,10 @@ impl Account {
             holder,
             balance: 0,
         }
+    }
+
+    fn summary(&self) -> String {
+        format!("{} has a balance {}", self.holder, self.balance)
     }
 
     fn deposit(&mut self, amount: i32) -> i32 {
@@ -49,5 +64,6 @@ fn main() {
 
     bank.add_account(account);
 
-    println!("{:#?}", bank)
+    println!("{:#?}", bank.summary());
+    println!("{}", bank.total_balance());
 }
