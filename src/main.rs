a2f1fcda13758950d8f363e38c323b7e7dc28d7c
Fikri Fadillah <fkrfdllh@gmail.com>
2025-02-14 14:08:02 +0700

    define media instead of structs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+/* Instead of doing this:
+struct Book {
+    title: String,
+    author: String,
+}
+
+struct Movie {
+    title: String,
+    director: String,
+}
+
+struct Audiobook {
+    title: String,
+}
+*/
+
+// Do this:
+#[derive(Debug)]
+enum Media {
+    Book { title: String, author: String },
+    Movie { title: String, author: String },
+    Audiobook { title: String },
+}
+
 fn main() {
     println!("Hello, world!");
 }
