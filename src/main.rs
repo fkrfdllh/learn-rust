1cab33acf644fe3fb476df986af1d7e3202789c7
Fikri Fadillah <fkrfdllh@gmail.com>
2025-02-19 11:13:28 +0700

    use of Result options and Error lib

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
-// file system library
-use std::fs;
+use std::io::Error;
 
 fn main() {
-    let text = fs::read_to_string("logs.txt");
+    match divide(5.0, 0.0) {
+        Ok(number) => println!("{}", number),
+        Err(err) => println!("{}", err),
+    }
+}
 
-    println!("{:#?}", text);
+fn divide(a: f64, b: f64) -> Result<f64, Error> {
+    if b == 0.0 {
+        return Err(Error::other("can't divide by 0"));
+    }
+
+    return Ok(a / b);
 }
