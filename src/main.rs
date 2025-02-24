d6dafc485848ef4eb4010a816b659a619a3196c0
Fikri Fadillah <fkrfdllh@gmail.com>
2025-02-24 16:38:57 +0700

    working with string split

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,25 @@ use std::fs;
 
 fn main() {
     match fs::read_to_string("./logs.txt") {
-        Ok(value) => println!("{}", value.len()),
+        Ok(value) => {
+            let errors = extract_errors(value.as_str());
+
+            println!("{:#?}", errors);
+        }
         Err(message) => println!("failed to read file: {}", message),
     }
+}
+
+fn extract_errors(text: &str) -> Vec<&str> {
+    let splitted_text = text.split("\n");
+
+    let mut results = vec![];
+
+    for line in splitted_text {
+        if line.starts_with("ERROR") {
+            results.push(line);
+        }
+    }
+
+    return results;
 }
