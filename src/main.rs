f663a290f380ec3d425c301fb9ea3ceeee3e4ec0
Fikri Fadillah <fkrfdllh@gmail.com>
2025-03-07 10:06:44 +0700

    using .expect() instead of match to handle Result

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,23 @@
 use std::fs;
 
 fn main() {
-    match fs::read_to_string("./logs.txt") {
-        Ok(value) => {
-            let errors = extract_errors(value.as_str());
+    // match fs::read_to_string("./logs.txt") {
+    //     Ok(value) => {
+    //         let errors = extract_errors(value.as_str());
 
-            match fs::write("./error.log", errors.join("\n")) {
-                Ok(..) => println!("error log stored"),
-                Err(error) => println!("failed to write error log caused by: {}", error),
-            }
-        }
-        Err(err) => println!("failed to read file: {}", err),
-    }
+    //         match fs::write("./error.log", errors.join("\n")) {
+    //             Ok(..) => println!("error log stored"),
+    //             Err(error) => println!("failed to write error log caused by: {}", error),
+    //         }
+    //     }
+    //     Err(err) => println!("failed to read file: {}", err),
+    // }
+
+    // these one is good to go too
+    // but instead of handling error, it will throw panic
+    let text = fs::read_to_string("./logs.txt").expect("failed to read logs.txt");
+    let errors = extract_errors(text.as_str());
+    fs::write("./logs/error.log", errors.join("\n")).expect("failed to write error.log");
 }
 
 fn extract_errors(text: &str) -> Vec<String> {
