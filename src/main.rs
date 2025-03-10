2f3db9104fe2bdb2d214f6291fdd4531baa8ddf2
Fikri Fadillah <fkrfdllh@gmail.com>
2025-03-10 09:47:43 +0700

    - main function can return Result - ? operator

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,15 @@
-use std::fs;
+use std::{fs, io::Error};
 
-fn main() {
-    // match fs::read_to_string("./logs.txt") {
-    //     Ok(value) => {
-    //         let errors = extract_errors(value.as_str());
+fn main() -> Result<(), Error> {
+    // let text = fs::read_to_string("./logs.txt").expect("failed to read logs.txt");
+    // let errors = extract_errors(text.as_str());
+    // fs::write("./logs/error.log", errors.join("\n")).expect("failed to write error.log");
 
-    //         match fs::write("./error.log", errors.join("\n")) {
-    //             Ok(..) => println!("error log stored"),
-    //             Err(error) => println!("failed to write error log caused by: {}", error),
-    //         }
-    //     }
-    //     Err(err) => println!("failed to read file: {}", err),
-    // }
-
-    // these one is good to go too
-    // but instead of handling error, it will throw panic
-    let text = fs::read_to_string("./logs.txt").expect("failed to read logs.txt");
+    let text = fs::read_to_string("./logs.txt")?;
     let errors = extract_errors(text.as_str());
-    fs::write("./logs/error.log", errors.join("\n")).expect("failed to write error.log");
+    fs::write("./error.log", errors.join("\n"))?;
+
+    Ok(())
 }
 
 fn extract_errors(text: &str) -> Vec<String> {
