5643318239e804ef283b809ae5dec6452663f1c0
Fikri Fadillah <fkrfdllh@gmail.com>
2025-03-15 20:42:33 +0700

    understanding .iter()

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
 fn main() {
-    println!("Hello, world!");
+    let colors = vec![
+        String::from("red"),
+        String::from("green"),
+        String::from("blue"),
+        String::from("yellow"),
+        String::from("white"),
+        String::from("black"),
+    ];
+
+    // iter is creating Iter<String> struct that pointing to source data
+    // it means if we wanna pointing to next data of the iterator
+    // then the color_iter will mutate (changing value of the pointer)
+    // to the next value of the iteration
+    let mut colors_iter = colors.iter();
+
+    println!("{:#?}", colors_iter.next());
+    println!("{:#?}", colors_iter.next());
+    println!("{:#?}", colors_iter.next());
+    println!("{:#?}", colors_iter.next());
+    println!("{:#?}", colors_iter.next());
+    println!("{:#?}", colors_iter.next());
 }
