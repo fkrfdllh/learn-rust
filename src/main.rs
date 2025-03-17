068d1d9c4f1263c33deeaabe17b95a5486b3dec1
Fikri Fadillah <fkrfdllh@gmail.com>
2025-03-17 09:03:12 +0700

    the problem

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    let a: f32 = 3.0;
+    let b = 4.0;
+    let c = solve(a, b);
+
+    println!("result of the pythagorean: {}", c);
+}
+
+fn solve(a: f64, b: f64) -> f64 {
+    return (a.powi(2) + b.powi(2)).sqrt();
 }
