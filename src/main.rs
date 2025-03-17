deff6aeb4afaea1f60956690ae4baabda639bcaa
Fikri Fadillah <fkrfdllh@gmail.com>
2025-03-17 09:27:24 +0700

    start using generic type <T>

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,21 @@
-use num_traits::ToPrimitive;
+use num_traits::{Float, ToPrimitive};
 
 fn main() {
     let a: f32 = 3.0;
-    let b: f64 = 4.0;
+    let b: f32 = 4.0;
 
-    // rust can not do calculation between different data type of number
+    // ::<f32> gonna replace <T> with <f32>
+    let c = solve::<f32>(a, b);
 
-    // let a_f64 = a as f64;
-    let a_f64 = a.to_f64().unwrap();
-
-    let c = solve(a_f64, b);
+    // not using ::<f32> gonna runs well too
+    // let c = solve(a, b);
 
     println!("result of the pythagorean: {}", c);
 }
 
-fn solve(a: f64, b: f64) -> f64 {
-    return (a.powi(2) + b.powi(2)).sqrt();
+fn solve<T: Float>(a: T, b: T) -> f64 {
+    let a64 = a.to_f64().unwrap();
+    let b64 = b.to_f64().unwrap();
+
+    return (a64.powi(2) + b64.powi(2)).sqrt();
 }
