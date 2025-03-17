006d0981b54ce13febe36ce13d9db3087c538091
Fikri Fadillah <fkrfdllh@gmail.com>
2025-03-17 09:59:27 +0700

    using multiple generic types

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,13 @@ use num_traits::{Float, ToPrimitive};
 
 fn main() {
     let a: f32 = 3.0;
-    let b: f32 = 4.0;
-
-    // ::<f32> gonna replace <T> with <f32>
-    let c = solve::<f32>(a, b);
-
-    // not using ::<f32> gonna runs well too
-    // let c = solve(a, b);
+    let b: f64 = 4.0;
+    let c = solve::<f32, f64>(a, b);
 
     println!("result of the pythagorean: {}", c);
 }
 
-fn solve<T: Float>(a: T, b: T) -> f64 {
+fn solve<T: Float, U: Float>(a: T, b: U) -> f64 {
     let a64 = a.to_f64().unwrap();
     let b64 = b.to_f64().unwrap();
 
