3538db0233283c56d12abd38c4737519b5bf5d09
Fikri Fadillah <fkrfdllh@gmail.com>
2025-03-17 10:02:03 +0700

    using ToPrimitive trait instead of Float trait as a generic type

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
-use num_traits::{Float, ToPrimitive};
+use num_traits::ToPrimitive;
 
 fn main() {
-    let a: f32 = 3.0;
+    let a: u32 = 3;
     let b: f64 = 4.0;
-    let c = solve::<f32, f64>(a, b);
+    let c = solve(a, b);
 
     println!("result of the pythagorean: {}", c);
 }
 
-fn solve<T: Float, U: Float>(a: T, b: U) -> f64 {
+fn solve<T: ToPrimitive, U: ToPrimitive>(a: T, b: U) -> f64 {
     let a64 = a.to_f64().unwrap();
     let b64 = b.to_f64().unwrap();
 
