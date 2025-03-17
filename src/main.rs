a67ffdf52b94109e981ab35cc9822e50ccc792b9
Fikri Fadillah <fkrfdllh@gmail.com>
2025-03-17 07:46:57 +0700

    use lifetime annotation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
 fn main() {
-    println!("Hello, world!");
+    let languages = vec![
+        String::from("PHP"),
+        String::from("JavaScript"),
+        String::from("Java"),
+        String::from("Golang"),
+        String::from("Rust"),
+        String::from("Python"),
+    ];
+
+    let result = next_language(&languages, "Python");
+
+    println!("{}", result);
+}
+
+// lifetime annotation used to mark
+// the output of the scope gonna use which reference
+// it will use
+fn next_language<'a, 'b>(languages: &'a [String], current: &'b str) -> &'a str {
+    let mut found = false;
+
+    for language in languages {
+        if found {
+            return language;
+        }
+
+        if language == current {
+            found = true;
+        }
+    }
+
+    return languages.last().unwrap();
 }
